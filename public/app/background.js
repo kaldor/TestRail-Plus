9a6f37d1cb8aab5f7846e84b4200b491c7ea1a5d
Steven Chen <stevenchenwaiho@gmail.com>
2024-05-10 23:44:30 +0100

    injection successful

--- public/app/background.js
+++ public/app/background.js
@@ -1,44 +1,60 @@
-
-
-const injectLinksToDOM = () => {
+const injectDOM = () => {
   const linksToOtherSuites = document.createElement("a")
   linksToOtherSuites.textContent = "Navigate to the other suite"
   linksToOtherSuites.href = "https://www.google.com"
+  linksToOtherSuites.id = "injected"
 
-  const injectLinksForSectionView = (document, element) => {
+  const injectAfterLoad = ({ document, parentElement, injectElement, injectToElementQuery, caseNameElementQuery }) => {
+
+    const inject = () => {
+      const injectToElement = document.querySelector(injectToElementQuery)
+      if (injectToElement) {
+        injectToElement.parentElement.insertBefore(injectElement, injectToElement)
+        console.log(document.querySelector(caseNameElementQuery).textContent)
+      }
+    }
+
+    // Try injecting before load
+    inject()
+
     const config = { attributes: true, childList: true, subtree: true }
 
-    var injected = false
     // Create an observer instance to add the injection after it is loaded
     var observer = new MutationObserver((mutationList, observer) => {
-      for (const mutation of mutationList) {
-        var injectToElement = document.querySelector("div.details-in-qpane")
-        if (injectToElement && mutation.type === "childList" && !injected) {
-          observer.disconnect();
-          injected = true
-          injectToElement.parentElement.insertBefore(linksToOtherSuites, injectToElement)
-          break
-        }
+      if (document.querySelector("#injected")) {
+        return
       }
+      inject()
     });
 
-    observer.observe(element, config);
+    observer.observe(parentElement, config);
   }
 
   // Try inserting it in the TestRail full screen view
   const fullScreenViewElement = document.querySelector("div.content-breadcrumb")
   if (fullScreenViewElement) {
-    var injectToElement = document.querySelector("div.io-container.io-frame")
-    injectToElement.parentElement.insertBefore(linksToOtherSuites, injectToElement)
-    console.log('Injected in containerFullScreenView YAYYY')
+    injectAfterLoad({
+      document: document,
+      parentElement: fullScreenViewElement,
+      injectElement: linksToOtherSuites,
+      injectToElementQuery: "div.io-container.io-framed",
+      caseNameElementQuery: "span.link-tooltip.content-header-title-tooltip"
+    })
+    console.log('Injected in Full Screen YAYYY')
     return
   }
 
   // Try inserting it in the TestRail section view
   const sectionViewElement = document.querySelector("div#qpane-content")
   if (sectionViewElement) {
-    injectLinksForSectionView(document, sectionViewElement)
-    console.log('Injected in containerSectionView YAYYY')
+    injectAfterLoad({
+      document: document,
+      parentElement: sectionViewElement,
+      injectElement: linksToOtherSuites,
+      injectToElementQuery: "div.details-in-qpane",
+      caseNameElementQuery: "span.link-tooltip.content-header-title-tooltip"
+    })
+    console.log('Injected in Section View YAYYY')
     return
   }
 
@@ -51,6 +67,6 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: injectLinksToDOM
+    func: injectDOM
   })
 });
