805a5d4cd96747845911b4bcee4f432f81a751ed
Steven Chen <stevenchenwaiho@gmail.com>
2024-05-14 20:10:59 +0100

    Can navigate to foreign case with checking section and comparing edit distances

--- public/app/background.js
+++ public/app/background.js
@@ -1,164 +1,226 @@
 function onPageLoad() {
-  const injectTestRail = () => {
+  // ===== Constants ======
+  const TESTRAIL_LINK = "TESTRAILLINK"
 
-    const TESTRAIL_LINK = "https://pugpig.testrail.net/index.php?/"
+  const EMAIL = "EMAIL"
+  const PASSWORD = "PASSWORD"
 
-    const EMAIL = "Email"
-    const PASSWORD = "Password"
+  const EDIT_DISTANCE_ALLOWANCE = 5
 
-    const EDIT_DISTANCE_ALLOWANCE = 5
+  const suiteList = [{ name: "Android", project_id: 1, suite_id: 1 }, { name: "iOS", project_id: 2, suite_id: 2 }, { name: "Web", project_id: 5, suite_id: 6 }, { name: "Timeline", project_id: 20, suite_id: 62 }]
 
-    const suiteList = [{ name: "iOS", project_id: 2, suite_id: 2 }, { name: "Web", project_id: 5, suite_id: 6 }, { name: "Timeline", project_id: 20, suite_id: 62 }]
-
-    const fetchForeignCase = async (suite, currentCase) => {
-      if (!suite || !currentCase) {
-        return
-      }
-
-      // Find foreign case by title
-      try {
-        const foreignCaseResponse = await fetch(TESTRAIL_LINK + `api/v2/get_cases/${suite.project_id}&suite_id=${suite.suite_id}&filter=${currentCase.name}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            'Authorization': 'Basic ' + btoa(`${EMAIL}:${PASSWORD}`)
-          }
-        })
-
-        console.log(foreignCaseResponse)
-
-        // Fetch Foreign case failed
-        if (!foreignCaseResponse.ok) {
-          const result = await foreignCaseResponse.text()
-          console.log(result)
-          throw result
-        }
-
-        const casesResult = await foreignCaseResponse.json().cases
-        console.log('Foreign case candidates: ', casesResult)
-
-        // If only one matches
-        if (casesResult.length === 1) {
-          return casesResult[0]
-        }
-
-        // Check edit distance
-        const editDistance = (s, t) => {
-          if (!s.length) return t.length;
-          if (!t.length) return s.length;
-          const arr = [];
-          for (let i = 0; i <= t.length; i++) {
-            arr[i] = [i];
-            for (let j = 1; j <= s.length; j++) {
-              arr[i][j] =
-                i === 0
-                  ? j
-                  : Math.min(
-                    arr[i - 1][j] + 1,
-                    arr[i][j - 1] + 1,
-                    arr[i - 1][j - 1] + (s[j - 1] === t[i - 1] ? 0 : 1)
-                  );
-            }
-          }
-          return arr[t.length][s.length];
-        };
-
-        if (editDistance(casesResult.cases[0], currentCase.name) <= EDIT_DISTANCE_ALLOWANCE) {
-          return casesResult.cases[0]
-        }
-
-        throw 'Missed case'
-
-      } catch (error) {
-        throw error
+  // ===== Helper Functions =====
+  const editDistance = (s, t) => {
+    if (!s.length) return t.length;
+    if (!t.length) return s.length;
+    const arr = [];
+    for (let i = 0; i <= t.length; i++) {
+      arr[i] = [i];
+      for (let j = 1; j <= s.length; j++) {
+        arr[i][j] =
+          i === 0
+            ? j
+            : Math.min(
+              arr[i - 1][j] + 1,
+              arr[i][j - 1] + 1,
+              arr[i - 1][j - 1] + (s[j - 1] === t[i - 1] ? 0 : 1)
+            );
       }
     }
+    return arr[t.length][s.length];
+  };
 
-    const openForeignCase = async (suite, currentCase) => {
-      var foreignCase
-      try {
-        foreignCase = await fetchForeignCase(suite, currentCase)
-        console.log('Foreign case: ', foreignCase)
-      } catch (error) {
-        alert('Fetch operation failed: ' + error)
-      }
-      const { id } = foreignCase
-      const foreignCaseLink = TESTRAIL_LINK + "cases/view/" + id
-      window.open(foreignCaseLink, "_blank");
-    }
-
-    const injectForeignTestCases = (suiteList, injectToElement, currentCase) => {
-      suiteList.forEach((suite) => {
-        const foreignCaseElement = document.createElement("a")
-        try {
-          foreignCaseElement.onclick = async () => await openForeignCase(suite, currentCase)
-          foreignCaseElement.textContent = suite.name
-          foreignCaseElement.style.marginRight = "10px";
-          injectToElement.appendChild(foreignCaseElement)
-        } catch (error) {
-          alert(error)
+  const fetchTestRailAPI = async (apiPath) => {
+    try {
+      const testcaseResponse = await fetch(TESTRAIL_LINK + `api/v2/${apiPath}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          'Authorization': 'Basic ' + btoa(`${EMAIL}:${PASSWORD}`)
         }
       })
+      if (!testcaseResponse.ok) {
+        throw 'API error: ' + testcaseResponse.text()
+      }
+      return testcaseResponse.json()
+    } catch (error) {
+      throw 'API error: ' + error
+    }
+  }
+
+  const fetchForeignCase = async (suite, currentCase) => {
+    if (!suite || !currentCase) {
+      return
     }
 
-    const injectAfterLoad = ({ document, parentElement, injectToElementQuery, caseIdElementQuery, caseNameElementQuery, injectLinkToCaseNumber }) => {
-
-      const injectElementContainer = document.createElement("div")
-      injectElementContainer.id = "injected"
-      injectElementContainer.style.marginBottom = "10px";
-
-      const inject = () => {
-        const injectToElement = document.querySelector(injectToElementQuery)
-        if (injectToElement) {
-          injectToElement.parentElement.insertBefore(injectElementContainer, injectToElement)
-
-          const caseNumberElement = document.querySelector(caseIdElementQuery)
-          const caseNameElement = document.querySelector(caseNameElementQuery)
-
-          if (!caseNumberElement || !caseNameElement) {
-            throw 'Could not get case name and id'
-          }
-
-          const currentCaseId = Number(caseNumberElement.textContent.trim().substring(1))
-          const currentCaseName = caseNameElement.textContent.trim()
-
-          const currentCase = { name: currentCaseName, id: currentCaseId }
-          console.log('Current case: ', currentCase)
-          injectElementContainer.innerHTML = ''
-
-          // Inject Link to case number
-          if (injectLinkToCaseNumber) {
-            const idLink = document.createElement("a")
-            idLink.id = "injectedIdLink"
-            idLink.href = TESTRAIL_LINK + "cases/view/" + currentCaseId
-            idLink.textContent = 'C' + currentCaseId
-            idLink.classList.add("nolink")
-
-            caseNumberElement.innerHTML = ''
-            caseNumberElement.appendChild(idLink)
-          }
-
-          // Inject Foreign Case
-          injectForeignTestCases(suiteList, injectElementContainer, currentCase)
+    // Find foreign case by title
+    try {
+      const foreignCaseResponse = await fetch(TESTRAIL_LINK + `api/v2/get_cases/${suite.project_id}&suite_id=${suite.suite_id}&filter=${currentCase.title}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          'Authorization': 'Basic ' + btoa(`${EMAIL}:${PASSWORD}`)
         }
+      })
+
+      console.log(foreignCaseResponse)
+
+      // Fetch Foreign case failed
+      if (!foreignCaseResponse.ok) {
+        const result = await foreignCaseResponse.text()
+        console.log(result)
+        throw result
       }
 
-      // Try injecting before load
-      inject()
+      const casesJson = await foreignCaseResponse.json()
+      console.log('Foreign Json: ', casesJson)
+      const casesResult = casesJson.cases
+      console.log('Foreign case candidates: ', casesResult)
 
-      const config = { attributes: true, childList: true, subtree: true }
+      // If no matches
+      if (casesResult.length === 0) {
+        alert('Cannot find match')
+        return null
+      }
 
-      // Create an observer instance to add the injection after it is loaded
-      var observer = new MutationObserver((mutationList, observer) => {
-        if (document.querySelector("#injected")) {
-          return
+      // If only one matches
+      if (casesResult.length === 1) {
+        return casesResult[0]
+      }
+
+      const casesWithExactSameName = casesResult.filter((test) => test.title === currentCase.title)
+
+      // Only one candidate with the exact title
+      if (casesWithExactSameName.length === 1) {
+        return casesWithExactSameName[0]
+      }
+
+      // Update current case with more information by calling get_case API
+      const currentCaseUpdated = await fetchTestRailAPI(`get_case/${currentCase.id}`)
+
+      // There are muliple candidates with the exact title
+      if (casesWithExactSameName.length > 1) {
+        // Check if section name match up
+        const currentCaseSection = await fetchTestRailAPI(`get_section/${currentCaseUpdated.section_id}`)
+        for (let candidate of casesWithExactSameName) {
+          const candidateSection = await fetchTestRailAPI(`get_section/${candidate.section_id}`)
+          if (currentCaseSection.name === candidateSection.name) {
+            return candidate
+          }
         }
-        inject()
-      });
+        return casesWithExactSameName[0]
+      }
 
-      observer.observe(parentElement, config);
+      // If no section name match up, get the best candidate by the details
+      const currentCaseDetails = currentCaseUpdated.title + currentCaseUpdated.custom_preconds + currentCaseUpdated.custom_steps + currentCaseUpdated.custom_expected
+
+      var minEditDistance = Infinity
+      var bestCandidate = null
+      casesResult.forEach((testcase) => {
+        const candidateDetails = testcase.title + testcase.custom_preconds + testcase.custom_steps + testcase.custom_expected
+        const totalEditDistance = editDistance(currentCaseDetails, candidateDetails)
+        if (minEditDistance > totalEditDistance) {
+          minEditDistance = totalEditDistance
+          bestCandidate = testcase
+        }
+      })
+
+      console.log(minEditDistance)
+
+      return bestCandidate
+
+    } catch (error) {
+      throw error
+    }
+  }
+
+  const openForeignCase = async (suite, currentCase) => {
+    var foreignCase
+    try {
+      foreignCase = await fetchForeignCase(suite, currentCase)
+      console.log('Foreign case: ', foreignCase)
+    } catch (error) {
+      alert('Fetch operation failed: ' + error)
+    }
+    const { id } = foreignCase
+    const foreignCaseLink = TESTRAIL_LINK + "cases/view/" + id
+    window.open(foreignCaseLink, "_blank");
+  }
+
+  const injectForeignTestCases = (suiteList, injectToElement, currentCase) => {
+    injectToElement.innerHTML = ''
+    suiteList.forEach((suite) => {
+      const foreignCaseElement = document.createElement("a")
+      try {
+        foreignCaseElement.onclick = async () => await openForeignCase(suite, currentCase)
+        foreignCaseElement.textContent = suite.name
+        foreignCaseElement.style.marginRight = "10px";
+        injectToElement.appendChild(foreignCaseElement)
+      } catch (error) {
+        alert(error)
+      }
+    })
+  }
+
+  const injectAfterLoad = ({ document, parentElement, injectToElementQuery, caseIdElementQuery, caseNameElementQuery, injectLinkToCaseNumber }) => {
+
+    const injectElementContainer = document.createElement("div")
+    injectElementContainer.id = "injected"
+    injectElementContainer.style.marginBottom = "10px";
+
+    const inject = () => {
+      const injectToElement = document.querySelector(injectToElementQuery)
+      if (injectToElement) {
+        injectToElement.parentElement.insertBefore(injectElementContainer, injectToElement)
+
+        const caseNumberElement = document.querySelector(caseIdElementQuery)
+        const caseNameElement = document.querySelector(caseNameElementQuery)
+
+        if (!caseNumberElement || !caseNameElement) {
+          throw 'Could not get case name and id'
+        }
+
+        const currentCaseId = Number(caseNumberElement.textContent.trim().substring(1))
+        const currentCaseTitle = caseNameElement.textContent.trim()
+
+        const currentCase = { title: currentCaseTitle, id: currentCaseId }
+        console.log('Current case: ', currentCase)
+        injectElementContainer.innerHTML = ''
+
+        // Inject Link to case number
+        if (injectLinkToCaseNumber) {
+          const idLink = document.createElement("a")
+          idLink.id = "injectedIdLink"
+          idLink.href = TESTRAIL_LINK + "cases/view/" + currentCaseId
+          idLink.textContent = 'C' + currentCaseId
+          idLink.classList.add("nolink")
+
+          caseNumberElement.innerHTML = ''
+          caseNumberElement.appendChild(idLink)
+        }
+
+        // Inject Foreign Case
+        injectForeignTestCases(suiteList, injectElementContainer, currentCase)
+      }
     }
 
+    // Try injecting before load
+    inject()
+
+    const config = { attributes: true, childList: true, subtree: true }
+
+    // Create an observer instance to add the injection after it is loaded
+    var observer = new MutationObserver((mutationList, observer) => {
+      if (document.querySelector("#injected")) {
+        return
+      }
+      inject()
+    });
+
+    observer.observe(parentElement, config);
+  }
+
+  const injectTestRail = () => {
     // Try inserting it in the TestRail full screen view
     const fullScreenViewElement = document.querySelector("div.content-breadcrumb")
     if (fullScreenViewElement) {
@@ -190,7 +252,6 @@ function onPageLoad() {
     }
 
   }
-
 
   injectTestRail()
 }
