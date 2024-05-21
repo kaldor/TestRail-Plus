c4a0431ac5dba4fbb81ee2426387b9deec12597d
Steven Chen <stevenchenwaiho@gmail.com>
2024-05-21 15:03:09 +0100

    Created Frontend Suite List

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { Box, Tab, Tabs, ThemeProvider, createTheme } from '@mui/material'
+import { Box, List, ListItem, ListItemText, Tab, Tabs, ThemeProvider, createTheme } from '@mui/material'
 
 
 function App() {
@@ -18,6 +18,14 @@ function App() {
     return object
   }
 
+  interface Suite {
+    name: string,
+    project_id: number,
+    suite_id: number
+  }
+
+  // const DEFAULT_SUITELIST: Suite[] = [{ name: "Android", project_id: 1, suite_id: 1 }, { name: "iOS", project_id: 2, suite_id: 2 }, { name: "Web", project_id: 5, suite_id: 6 }, { name: "Timeline", project_id: 20, suite_id: 62 }]
+
   const [link_, setLink] = useState("")
   const [email_, setEmail] = useState("")
   const [token_, setToken] = useState("")
@@ -25,11 +33,11 @@ function App() {
 
   const firstLoad = async () => {
 
-    const { link, email, token } = isEmptyObjectReturnNull(await chrome.storage.local.get(["link", "email", "token"]))
+    const { link, email, token } = isEmptyObjectReturnNull(await chrome.storage.local.get(["link", "email", "token", "suites"]))
 
-    setLink(link)
-    setEmail(email)
-    setToken(token)
+    setLink(link || "")
+    setEmail(email || "")
+    setToken(token || "")
 
   }
   useEffect(() => {
@@ -72,14 +80,81 @@ function App() {
 
   const SuitesSettings = ({
     tab,
-    index
+    index,
   }: TabProps) => {
     if (tab !== index) {
       return (<></>)
     }
 
+    const [suites_, setSuites] = useState<Suite[]>([])
+
+    const firstLoad = async () => {
+      const { suites } = isEmptyObjectReturnNull(await chrome.storage.local.get(["link", "email", "token", "suites"]))
+      setSuites(suites || [])
+    }
+
+    useEffect(() => {
+      firstLoad()
+    })
+
+
+    const [addingSuite, setAddingSuite] = useState(false)
+
+    const [editSuiteName, setEditSuiteName] = useState("")
+    const [editProjectID, setEditProjectID] = useState(0)
+    const [editSuiteID, setEditSuiteID] = useState(0)
+
+    const removeByIndex = (list: Suite[], i: number) => {
+      const newList = list
+      newList.splice(i, 1)
+      return newList
+    }
+
+    const onRemoveSuite = (i: number) => {
+      const newList = removeByIndex(suites_, i)
+      chrome.storage.local.set({ suites: newList })
+      setSuites(newList)
+    }
+
+    const resetFields = () => {
+      setAddingSuite(false)
+      setEditSuiteName("")
+      setEditProjectID(0)
+      setEditSuiteID(0)
+    }
+
+    const onSubmitSuite = (newSuite: Suite) => {
+      const newSuiteList = [...suites_, newSuite]
+      chrome.storage.local.set({ suites: newSuiteList })
+      setSuites(newSuiteList)
+      resetFields()
+    }
+
     return (
       <>
+        <div></div>
+        {!addingSuite ? <h3><button onClick={() => { setAddingSuite((prev) => !prev) }}>+</button> Add Suite</h3>: <></>}
+        {addingSuite ?
+          <>
+            <h4>Suite Name</h4>
+            <input type='text' placeholder='Enter Suite Name here...' value={editSuiteName} onChange={(event) => setEditSuiteName(event.target.value)}></input>
+            <h4>Project ID</h4>
+            <input type='number' value={editProjectID} onChange={(event) => setEditProjectID(Number(event.target.value))}></input>
+            <h4>Suite ID</h4>
+            <input type='number' value={editSuiteID} onChange={(event) => setEditSuiteID(Number(event.target.value))}></input>
+            <div></div>
+            <button type='submit' onClick={() => onSubmitSuite({ name: editSuiteName, project_id: editProjectID, suite_id: editSuiteID })}>Add</button>
+          </>
+          : <></>}
+            <hr></hr>
+        <List>
+          {suites_.map((suite, i) => (
+            <ListItem key={i}>
+              <ListItemText primary={suite.name} secondary={`Project: ${suite.project_id} Suite: ${suite.suite_id}`}/>
+              <button onClick={() => onRemoveSuite(i)}>Remove</button>
+            </ListItem>
+          ))}
+        </List>
 
       </>
     )
@@ -104,7 +179,7 @@ function App() {
         </Box>
 
         <AccountSettings tab={tab} index={0} />
-        <SuitesSettings tab={tab} index={0} />
+        <SuitesSettings tab={tab} index={1} />
       </ThemeProvider>
     </>
   )
